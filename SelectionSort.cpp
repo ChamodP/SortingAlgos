b64729c46ca11ee335e7670bf9290e7e9b17793d
Your Name <you@example.com>
2023-10-30 11:09:22 +0530

    Added more test cases for  Selection Sort

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -25,7 +25,7 @@ void selectionSort(int arr[], int array_size)
     }
 }
 
-void printArray(int arr[],  int array_size)
+void printArray(int arr[], int array_size)
 {
 
     cout << "Array elements are : ";
@@ -40,14 +40,24 @@ void printArray(int arr[],  int array_size)
 
 int main()
 {
-    int array[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int testArr[][9] = {
+        {1, 2, 3, 4, 5, 6, 7, 8, 9},
+        {9, 8, 7, 6, 5, 4, 3, 2, 1},
+        {4, 2, 3, 4, 5, 4, 1, 2, 5},
+        {5, 5, 5, 5, 5, 5, 5, 5, 5},
+        {64, 25, 12, 22, 11, 36, 98, 43, 87}};
 
-    int array_size = sizeof(array) / sizeof(array[0]);
-    
-    cout<<"---- Befor Sorting ----"<<endl;
-    printArray(array,array_size);
+    for (int i = 0; i < 5; i++)
+    {
+        int array_size = sizeof(testArr[i]) / sizeof(testArr[i][0]);
 
-    cout<<"---- After Sorting ----"<<endl;
-    selectionSort(array, array_size);
-    printArray(array,array_size);
+        cout << "---- Befor Sorting ----" << endl;
+        printArray(testArr[i], array_size);
+
+        cout << "---- After Sorting ----" << endl;
+        selectionSort(testArr[i], array_size);
+        printArray(testArr[i], array_size);
+
+        cout<<endl;
+    }
 }
